c8e6000bbd2860ac976af7725de04100b058951f
frisby <32783481+sellersd@users.noreply.github.com>
2024-01-30 11:56:19 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# APCSA
+A project investigating The Sieve of Eratothenes using Java and arrays.
---
+++ Sieve.java
@@ -0,0 +1,14 @@
+/* Name
+ * 2024-01-30
+ * Description: The Sieve of Eratosthenes was a primitive method for 
+ * finding prime numbers. It is remarkable in its efficiency and simplicity.
+ * You are to implement The Sieve of Eratosthenes in Java using an one-dimensional
+ * array. You should prompt the user for an natural number and you should output
+ * all primes less than or equal to the given number.
+ */
+
+class Sieve {
+  public static void main(String[] args) {
+
+  }
+}
